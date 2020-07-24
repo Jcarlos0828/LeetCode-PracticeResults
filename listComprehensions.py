4a4883b5ca0e0eb8ea3a6804bd5556297af53b77
José Carlos Del Castillo <jose.carlos4013@gmail.com>
2020-07-23 19:21:25 -0500

    Understanding list comprehensions, not finished yet

---
+++ listComprehensions.py
@@ -0,0 +1,21 @@
+x = [2,7,11]
+#newList = [[i for i in range(x[0],x[len(x)-1]+1)],[-1 for i in range(x[0],x[len(x)-1]+1)]]
+#newList = [(i,-1) for i in range(x[0],x[len(x)-1]+1)]
+#print(newList)
+
+DIAL_CODES = [ 
+  (86, 'China'),
+  (91, 'India'),
+  (1, 'United States'),
+  (62, 'Indonesia'),
+  (55, 'Brazil'),
+  (92, 'akistan'),
+  (880, 'Bangladesh'),
+  (234, 'Nigeria'),
+  (7, 'Russia'),
+  (81, 'Japan'),
+]
+#country_code = {country: code for code, country in DIAL_CODES}
+#print(country_code)
+a = [1] *5
+print(a)
